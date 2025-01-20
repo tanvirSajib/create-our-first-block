057d994ca73bc9c8179a24ccb828cacdb0b9e862
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-01-20 12:10:52 +0600

    11. Returning React Components in the Edit & Save Functions

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var registerBlockType = wp.blocks.registerBlockType;
+var createElement = wp.element.createElement;
 
-registerBlockType('blocks-course/firstblock', {
-    edit: function() {
-        return "Edit";
-    } ,
-    save: function(){
-        return "Save";
-    }
+registerBlockType("blocks-course/firstblock", {
+  edit: function () {
+    return createElement(
+      "p",
+      {
+        className: "class",
+      },
+      "Edit"
+    );
+  },
+  save: function () {
+    return createElement("p", null, "Save");
+  },
 });
