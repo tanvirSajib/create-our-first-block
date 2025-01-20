32a1982d87e4c5698fd99b0476d5660b9a31fb26
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-01-20 12:04:27 +0600

    10. Registering our First Block

--- firstblock.php
+++ firstblock.php
@@ -7,3 +7,11 @@
 * Author: Tanvir
 * Author URI: https://tanvir.com
 */
+
+function blocks_course_first_block_init(){
+    register_block_type_from_metadata( __DIR__ );
+}
+
+add_action('init', 'blocks_course_first_block_init');
+
+
